5e8671b5af66ec0b820da8bb61e6b567a3f08658
arfraeeel1 <20241506@nbsc.edu.ph>
2026-08-24 20:25:33 +0800

    Updated default value for activity_log_status

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS activity_logs(
     user_id VARCHAR(50),
     user_email VARCHAR(50),
     activity_log_action VARCHAR(50) NOT NULL,
-    activity_log_status ENUM('success', 'failed') DEFAULT 'success',
+    activity_log_status ENUM('success','failed') DEFAULT 'success',
 
     -- Client parameters
     activity_log_client_ip VARCHAR(50),
